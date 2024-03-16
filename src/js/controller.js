343ee2216775dfe5ba3c3680a39310ad79ef07f2
arommers <arommers@codam.student.nl>
2024-03-16 11:22:22 +0100

    update after a while

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,11 +1,19 @@
 import * as model from './model.js'
 import recipeView from './views/recipeView.js'
 import searchView from './views/searchView.js'
+import resultsView from './views/resultsView.js'
+import paginationView from './views/paginationView.js'
+import bookmarksView from './views/bookmarksView.js'
 
 // polyfilling async/await
 import 'regenerator-runtime/runtime'
 // polyfilling everything else
 import 'core-js/stable';
+
+// if (module.hot)
+// {
+//   module.hot.accept()
+// }
 
 const controlRecipes = async function()
 {
@@ -14,14 +22,20 @@ const controlRecipes = async function()
     const id = window.location.hash.slice(1);
     if(!id)
       return ;
-    console.log(id);
     recipeView.renderSpinner();
 
-    // 1. Loading recipe
+    // 0. update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+    
+    // 1. update bookmarks view
+    bookmarksView.update(model.state.bookmarks);
+
+    // 2. Loading recipe
     await model.loadRecipe(id);
     
-    // 2. Rendering recipe
+    // 3. Rendering recipe
     recipeView.render(model.state.recipe);
+
   }
   catch(error)
   {
@@ -35,12 +49,22 @@ const controlSearchResults = async function()
 {
   try
   {
+    resultsView.renderSpinner();
+    
+    // 1. get search query
     const query = searchView.getQuery();
     if(!query)
       return;
     
+    // 2. load results 
     await model.loadSearchResults(query);
-    console.log(model.state.search.results);
+
+    // 3. render results
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(1));
+
+    // 4. render initial pagination button
+    paginationView.render(model.state.search)
   }
   catch(error)
   {
@@ -48,10 +72,54 @@ const controlSearchResults = async function()
   }
 }
 
+const controlPagination = function(goToPage)
+{
+    // render  new results
+    resultsView.render(model.getSearchResultsPage(goToPage));
+
+    // render new paginations button
+    paginationView.render(model.state.search)
+}
+
+const controlServings = function(newServings)
+{
+  // update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+}
+
+const controlAddBookmark = function()
+{
+ 
+  // 1. add or remove bookmarks
+  if(!model.state.recipe.bookmarked)
+    model.addBookmark(model.state.recipe);
+  else
+    model.deleteBookmark(model.state.recipe.id);
+
+  // 2. update recipe view
+  recipeView.update(model.state.recipe);
+
+  // 3. render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+}
+
+const controlBookmarks = function()
+{
+  bookmarksView.render(model.state.bookmarks);
+}
+
 const init = function()
 {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandleClick(controlPagination);
 }
 
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,5 @@
 import {async} from 'regenerator-runtime';
-import {API_URL} from './config.js';
+import {API_URL, RES_PER_PAGE} from './config.js';
 import {getJSON} from './helpers.js';
 
 export const state =
@@ -9,7 +9,10 @@ export const state =
     {
         query: '',
         results: [],
-    }
+        page: 1,
+        resultsPerPage: RES_PER_PAGE,
+    },
+    bookmarks: [],
 };
 
 export const loadRecipe = async function(id)
@@ -30,7 +33,11 @@ export const loadRecipe = async function(id)
             cookingTime: recipe.cooking_time,
             ingredients: recipe.ingredients
         };
-        console.log(state.recipe);
+
+        if(state.bookmarks.some(bookmark => bookmark.id === id))
+            state.recipe.bookmarked = true;
+        else
+            state.recipe.bookmarked = false;
     }
     catch(error)
     {
@@ -54,10 +61,74 @@ export const loadSearchResults= async function(query)
                 image: recipe.image_url,
             };
         });
+        state.search.page = 1;
     }
     catch(error)
     {
         console.error(`${error} -> our own error`);
         throw error;
     }
-}
+};
+
+export const getSearchResultsPage = function(page = state.search.page)
+{
+    state.search.page = page;
+    
+    const start = (page - 1) * state.search.resultsPerPage;
+    const end = page * state.search.resultsPerPage;
+    
+    return state.search.results.slice(start, end);
+};
+
+export const updateServings = function(newServings)
+{
+    state.recipe.ingredients.forEach(ing =>
+    {
+        // newqt = oldqt * newserv / oldserv // 2* 8 / 4
+        ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+    });
+    state.recipe.servings = newServings;
+};
+
+const   persistBookmarks = function()
+{
+    localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+};
+
+export const addBookmark = function(recipe)
+{
+    // add bookmark
+    state.bookmarks.push(recipe);
+
+    // Mark current recipe as bookmarked
+    if(recipe.id === state.recipe.id)
+        state.recipe.bookmarked = true;
+
+    persistBookmarks();
+};
+
+export const deleteBookmark = function(id)
+{
+    // delete bookmark
+    const index = state.bookmarks.findIndex(el => el.id === id);
+    state.bookmarks.splice(index, 1);
+
+    if(id === state.recipe.id)
+        state.recipe.bookmarked = false
+
+    persistBookmarks();
+};
+
+const init = function()
+{
+    const storage = localStorage.getItem('bookmarks');
+    if(storage)
+        state.bookmarks = JSON.parse(storage);
+};
+
+init();
+
+// const clearBookmarks = function()
+// {
+//     localStorage.clear('bookmarks');
+// }
